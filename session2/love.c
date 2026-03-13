36a74d1a08f95bddf121549d3ceea6b2fc30ff7b
Jiaqi-stu <cdld0656@leeds.ac.uk>
2026-03-13 12:58:48 +0000

    finish

---
+++ session2/love.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char answer;
+    printf("Do you like me?\n");
+    scanf("%c", &answer);
+    if(answer == 'y'){
+        printf("520 1314\n");
+    }else{
+        printf("666,ewwwwwwwwwwwwwwwwwwwwwwwww");
+    }
+    return 0;
+}
